ce69f7f3ce942b2a6e4795391112743f83a7527b
MelvDouc <melv.douc@gmaiL.com>
2022-03-05 21:00:42 +0100

    documented Application methods

--- src/core/Application.php
+++ src/core/Application.php
@@ -5,19 +5,28 @@ namespace TestApp\Core;
 class Application
 {
   public static string $ROOT_DIR;
-  public static string $HTTP_HOST;
   public static Application $instance;
 
+  /**
+   * @return Database The database instance of the current application.
+   */
   public static function getDb(): Database
   {
     return self::$instance->db;
   }
 
+  /**
+   * joinPaths("views, index.twig") => "[root]/views/index.twig"
+   */
   public static function joinPaths(...$pathSegments): string
   {
     return implode("/", [self::$ROOT_DIR, ...$pathSegments]);
   }
 
+  /**
+   * @param string $routeName - Matches the first argument of `(new Router())->addRoute()`.
+   * @param array $context - An optional associative array containing the placeholders in the route name as keys and their corresponding values. 
+   */
   public static function getFullRoute(string $routeName, array $context = [])
   {
     return "http://localhost:5000/" . Path::addContext(
@@ -32,11 +41,13 @@ class Application
   public function __construct(string $ROOT_DIR)
   {
     self::$ROOT_DIR = $ROOT_DIR;
-    self::$HTTP_HOST = getenv("HTTP_HOST");
     self::$instance = $this;
     $this->db = new Database();
   }
 
+  /**
+   * Add all of a router's routes to this instance's routes.
+   */
   public function useRouter(Router $router): Application
   {
     $this->routes = array_merge($this->routes, $router->getRoutes());
@@ -64,9 +75,7 @@ class Application
         return;
       }
 
-      $params = Path::getParamsMap($dynamicPath, $staticPath);
-      $req->setParams($params);
-
+      $req->setParams(Path::getParamsMap($dynamicPath, $staticPath));
       call_user_func($route["methods"][$httpMethod], $req, $res);
       return;
     }
