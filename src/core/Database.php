1d4a5defcd002c5ce9c966db08dacb3b9acea517
unknown <melv.douc@gmail.com>
2022-03-06 03:00:48 +0100

    added delete method

--- src/core/Database.php
+++ src/core/Database.php
@@ -7,6 +7,8 @@ use PDOStatement;
 
 class Database
 {
+  private const PLACEHOLDERS_KEY = "placeholders";
+  private const VALUES_KEY = "values";
   private static array $comparers = [">", ">=", "<", "<=", "LIKE"];
 
   private static function escapeQuery(array $filter, string $comparer = "=", string $logicalOperator = null): array
@@ -23,15 +25,15 @@ class Database
     foreach ($filter as $key => $value) {
       if (in_array($key, self::$comparers)) {
         $result = self::escapeQuery($value, $key, $logicalOperator);
-        array_push($placeholders, $result["placeholders"]);
-        array_push($values, ...$result["values"]);
+        array_push($placeholders, $result[self::PLACEHOLDERS_KEY]);
+        array_push($values, ...$result[self::VALUES_KEY]);
         continue;
       }
 
       if ($key === "AND" || $key === "OR") {
         $result = self::escapeQuery($value, $comparer, $key);
-        array_push($placeholders, $result["placeholders"]);
-        array_push($values, ...$result["values"]);
+        array_push($placeholders, $result[self::PLACEHOLDERS_KEY]);
+        array_push($values, ...$result[self::VALUES_KEY]);
         continue;
       }
 
@@ -41,6 +43,13 @@ class Database
 
     $placeholders = implode(($logicalOperator ? " $logicalOperator " : ""), $placeholders);
     return compact("placeholders", "values");
+  }
+
+
+  private static function bindValues(PDOStatement &$statement, array $values): void
+  {
+    for ($i = 1; $i <= count($values); $i++)
+      $statement->bindValue($i, $values[$i - 1]);
   }
 
   public \PDO $db;
@@ -78,18 +87,12 @@ class Database
     return $this->db->prepare($sql);
   }
 
-  private function bindValues(PDOStatement &$statement, array $values)
-  {
-    for ($i = 1; $i <= count($values); $i++)
-      $statement->bindValue($i, $values[$i - 1]);
-  }
-
   public function getOne(string $tableName, array $filter)
   {
     $whereClause = self::escapeQuery($filter);
-    $placeholders = $whereClause["placeholders"];
+    $placeholders = $whereClause[self::PLACEHOLDERS_KEY];
     $statement = $this->prepare("SELECT * FROM $tableName WHERE $placeholders");
-    $this->bindValues($statement, $whereClause["values"]);
+    self::bindValues($statement, $whereClause[self::VALUES_KEY]);
 
     $statement->execute();
     return $statement->fetch();
@@ -98,10 +101,10 @@ class Database
   public function getAll(string $tableName, array $columns = ["*"], array $filter = []): array
   {
     $whereClause = self::escapeQuery($filter);
-    $placeholders = $whereClause["placeholders"];
+    $placeholders = $whereClause[self::PLACEHOLDERS_KEY];
     $columns = implode(", ", $columns);
     $statement = $this->prepare("SELECT $columns FROM $tableName WHERE $placeholders");
-    $this->bindValues($statement, $whereClause["values"]);
+    self::bindValues($statement, $whereClause[self::VALUES_KEY]);
 
     $statement->execute();
     return $statement->fetchAll();
@@ -115,7 +118,7 @@ class Database
     $statement = $this->prepare(
       "INSERT INTO $tableName ($columns) VALUES ($placeholders)"
     );
-    $this->bindValues($statement, array_values($keyValuePairs));
+    self::bindValues($statement, array_values($keyValuePairs));
     return $statement->execute();
   }
 
@@ -123,12 +126,23 @@ class Database
   {
     $setClause = self::escapeQuery($updates, "=", ",");
     $whereClause = self::escapeQuery($filter);
-    $columns = $setClause["placeholders"];
-    $wherePlaceholders = $whereClause["placeholders"];
-    $values = array_merge($setClause["values"], $whereClause["values"]);
+    $columns = $setClause[self::PLACEHOLDERS_KEY];
+    $placeholders = $whereClause[self::PLACEHOLDERS_KEY];
 
-    $statement = $this->prepare("UPDATE $tableName SET $columns WHERE $wherePlaceholders");
-    $this->bindValues($statement, $values);
+    $statement = $this->prepare("UPDATE $tableName SET $columns WHERE $placeholders");
+    self::bindValues(
+      $statement,
+      array_merge($setClause[self::VALUES_KEY], $whereClause[self::VALUES_KEY])
+    );
+    return $statement->execute();
+  }
+
+  public function delete(string $tableName, array $filter): bool
+  {
+    $whereClause = self::escapeQuery($filter);
+    $placeholders = $whereClause[self::PLACEHOLDERS_KEY];
+    $statement = $this->prepare("DELETE FROM $tableName WHERE $placeholders");
+    self::bindValues($statement, $whereClause[self::VALUES_KEY]);
     return $statement->execute();
   }
 }
